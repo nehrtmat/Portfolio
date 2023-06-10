6d28c8ff8e0fc9b2e268a91a44a77dfce1cbff10
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-06-10 18:54:22 +0530

    Nav bar made

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import './App.css'
+import { BrowserRouter as Router } from "react-router-dom";
+import { Navbar } from './components';
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+			<div className="">
+				<div>
+					<Navbar />
+					{/* <Hero /> */}
+				</div>
+				{/* <About />
+				<Tech />
+				<Works />
+				<div className="relative z-0">
+					<Contact />
+				</div> */}
+			</div>
+		</Router>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { logo, menu, close } from '../assets';
+
+
+const Navbar = () => {
+	const [active, setActive] = useState("");
+	const [toggle, setToggle] = useState(false);
+    const navLinks = ["About", "Skills", "Contact"];
+
+	return (
+		<nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+			<div className='w-full flex justify-between items-center max-w7xl mx-auto'>
+				<Link to='/' className='flex items-center gap-2'
+					onClick={() => {
+						setActive("");
+						window.scrollTo(0, 0);
+					}}>
+					<img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+					<p className='text-white text-[18px] font-bold cursor-pointer'>Aayush Gupta</p>
+				</Link>
+				<ul className='list-none hidden sm:flex flex-row gap-10'>
+					{navLinks.map((link, linkIdx) => (
+						<li key={linkIdx} className={`${active === link ? "text-white" : "text-secondary" }
+							hover:text-white text-[18px] font-medium cursor-pointer`}
+							onClick={() => setActive(link)}>
+							<a href={`#${link.toLowerCase()}`}>{link}</a>
+						</li>
+					))}
+				</ul>
+				<div className='sm:hidden flex flex-1 justify-end items-center'>
+					<img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer'
+					onClick={() => setToggle(!toggle)}/>
+					<div className={`${toggle ? "flex": "hidden"} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+						<ul className='list-none flex justify-end items-start flex-col gap-4'>
+							{navLinks.map((link, linkIdx) => (
+								<li key={linkIdx} className={`${active === link ? "text-white" : "text-secondary" }
+									font-poppins font-medium cursor-pointer text-[16px]`}
+									onClick={() => {
+										setToggle(false);
+										setActive(link);
+										}}>
+									<a href={`#${link.toLowerCase()}`}>{link}</a>
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+ 	)
+}
+
+export default Navbar;
