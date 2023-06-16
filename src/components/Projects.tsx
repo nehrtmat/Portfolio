f9c8d3e7fbdfa07d64e5f9289ffe0249792a980e
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-06-17 04:01:19 +0530

    Updated Projects list

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,13 @@
-// import { useContext } from 'react';
-// import { context } from "../App";
-
+import { useContext } from 'react';
+import { styles } from "../styles";
+import { context } from "../App";
 
 const Projects = () => {
-    // const { isLight } = useContext(context);
+    const { isLight } = useContext(context);
 
     return (
-        <div id="projects" className="text-[#c3bcbd]">
+        <div id="projects" className={`mt-4 ${styles.paddingX} max-w-7xl mx-auto flex flex-col items-start justify-between`}>
+            <p className={`${styles.sectionSubText} ${isLight ? "text-black-200" : "text-white-100" } mt-10 font-semibold`}>What I made</p><h2 className={`${styles.sectionHeadText} ${isLight ? "text-black-100" : "text-white-100"}`}>Projects.</h2>
             
         </div>
     )
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -31,3 +31,30 @@ export const languages = [
     {title: 'SQL', icon: sql }, 
     {title: 'MongoDB', icon: mongodb }
 ];
+
+export const projects = [
+    {
+        title: 'Portfolio', 
+        description: 'My humble portfolio website', 
+        link: 'https://aayush65.com',
+        source: "https://github.com/Aayush65/Portfolio"
+    },
+    {
+        title: 'Wordle2', 
+        description: "A game inspired by the NYT's Wordle Game",
+        link: 'https://wordle2.aayush65.com',
+        source: "https://github.com/Aayush65/Wordle2"
+    },
+    {
+        title: 'Flip Game', 
+        description: 'A Memory based Game made up of fliped tiles which has some image on their rear side. One has to match cards bearing identical images with least amount of flips to win the game.', 
+        link: 'https://flipgame.aayush65.com',
+        source: "https://github.com/Aayush65/flipgame"
+    },
+    {
+        title: 'Sudoku', 
+        description: 'A Sudoku Website. This website can generate all levels of unfilled sudokus to play with alongwith the ability to complete any possible unfilled sudoku. It also helps the player along the way by highlighting possible conflicts while cell filling.', 
+        link: 'https://sudoku.aayush65.com',
+        source: "https://github.com/Aayush65/sudoku"
+    },
+]
