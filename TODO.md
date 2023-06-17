8172e35933ce0adb6a77db80d4e7f2e162148f8a
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-06-18 03:12:19 +0530

    Minor Adjustments

---
+++ TODO.md
@@ -0,0 +1,3 @@
+1. Add technologies below project description + Links
+2. Add footer scoial media links
+4. Add a ??
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
 	return (
 		<nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 ${ isLight ? "bg-white" : "bg-black" } bg-opacity-60 backdrop-blur`}>
-			<div className='w-full flex justify-between items-center max-w7xl mx-auto'>
+			<div className='w-full flex justify-between items-center mx-auto'>
 				<Link to='/' className='flex items-center gap-2'
 					onClick={() => {
 						setActive("");
@@ -26,7 +26,7 @@ const Navbar = () => {
 					<div className={`bg-gray-700 rounded-full p-1 none ${isLight ? 'invisible': null}`}>🌚</div>
 				</button>
 				
-				<ul className='list-none hidden sm:flex flex-row gap-10'>
+				<ul className='list-none hidden sm:flex flex-row gap-10 justify-center items-center'>
 					{navLinks.map((link, linkIdx) => (
 						<li key={linkIdx} className={`${active === link && isLight ? `text-secondary-light`: active === link ? `text-secondary-dark`: isLight ? `text-black-100`: `text-white-100`} hover:opacity-75 text-[18px] font-medium cursor-pointer`}
 							onClick={() => setActive(link)}>
