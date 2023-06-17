8e51f9994fe60b45d53f74caaf57596935732328
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-06-17 14:48:17 +0530

    Added Projects

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -24,7 +24,7 @@ const About = () => {
     const { isLight } = useContext(context);
 
     return (
-        <div id="about" className={`mt-4 ${styles.paddingX} max-w-7xl mx-auto flex flex-col items-start justify-between`}>
+        <div id="about" className={`mt-10 ${styles.paddingX} max-w-7xl mx-auto flex flex-col items-start justify-between`}>
             <p className={`${styles.sectionSubText} ${isLight ? "text-black-200" : "text-white-100" } mt-10 font-semibold`}>About Me</p>
             <h2 className={`${styles.sectionHeadText} ${isLight ? "text-black-200" : "text-white-100" }`}>Introduction.</h2>
             <div className={`${isLight ? "text-secondary-text-light" : "text-secondary-dark" } text-[17px] max-w-3xl leading-[30px] md:ml-3`}>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,30 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { styles } from "../styles";
 import { context } from "../App";
+import { projects } from '../constants';
+
 
 const Projects = () => {
     const { isLight } = useContext(context);
+    const [ active, setActive ] = useState<number>(0); 
 
     return (
-        <div id="projects" className={`mt-4 ${styles.paddingX} max-w-7xl mx-auto flex flex-col items-start justify-between`}>
-            <p className={`${styles.sectionSubText} ${isLight ? "text-black-200" : "text-white-100" } mt-10 font-semibold`}>What I made</p><h2 className={`${styles.sectionHeadText} ${isLight ? "text-black-100" : "text-white-100"}`}>Projects.</h2>
-            
+        <div id="projects" className="mt-4">
+            <div className={`${styles.paddingX} max-w-7xl mx-auto flex flex-col items-start justify-between`}>
+                <p className={`${styles.sectionSubText} ${isLight ? "text-black-200" : "text-white-100" } mt-10 font-semibold`}>What I made</p><h2 className={`${styles.sectionHeadText} ${isLight ? "text-black-100" : "text-white-100"} mb-16`}>Projects.</h2>
+                <div className='flex justify-between items-center gap-10'>
+                    <div className={`p-4 border-4 ${isLight ? "border-primary-dark" : "border-primary-light"} w-full md:w-1/2`}>
+                        {projects.map((project, index) => (
+                            <div key={index} className={`p-4 border-2 ${isLight ? "border-primary-dark" : "border-primary-light"} flex flex-col items-start justify-center`} onClick={() => setActive(index)}>
+                                <h3 className={`${styles.sectionSubText} ${isLight ? "text-black-200" : "text-white-100" }`}>{project.title}</h3>
+                                <p className={`${isLight ? "text-secondary-text-light" : "text-secondary-dark" } text-[17px] max-w-3xl leading-[30px] md:ml-3`}>{project.description}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <iframe src={projects[active].link} className={`hidden sm:block w-[400px] h-[80vh] border-2 ${isLight ? "border-primary-dark" : "border-primary-light"} rounded-xl mb-5`} > Sorry Error!!</iframe>
+                </div>
+            </div>
+            <iframe src={projects[active].link} className={`my-10 md:hidden w-screen md:w-[400px] h-[80vh] border-2 ${isLight ? "border-primary-dark" : "border-primary-light"} rounded-xl mb-5`} > Sorry Error!!</iframe>
         </div>
     )
 }
